9631de5f7b4bfd183ca889151e1fe318aa31d974
Rafael-Fernandes314 <rafaelfernandesjp@gmail.com>
2025-05-31 15:22:33 -0300

    Adicionado as movimentações, ccenário e colisão com o chão

--- Scripts/jogo.py
+++ Scripts/jogo.py
@@ -5,30 +5,45 @@ from player import Eindein
 
 pygame.init()
 
-largura = 640
+largura = 840
 altura = 480
 
-PRETO = (0,0,0)
+PRETO = (0, 0, 0)
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Sprites")
+pygame.display.set_caption("Jogo")
 
-todas_as_sprites = pygame.sprite.Group()
+fundo_img = pygame.image.load("Assets/Sprites/Cenários/fase-1.png").convert()
+fundo_img = pygame.transform.scale(fundo_img, (200 * 6.5, 75 * 6.5))
+fundo_largura = 1300
+
+sprites = pygame.sprite.Group()
 eindein = Eindein()
-todas_as_sprites.add(eindein)
+sprites.add(eindein)
 
 relógio = pygame.time.Clock()
-        
+
 while True:
     relógio.tick(30)
-    tela.fill((PRETO))
+    tela.fill(PRETO)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         if event.type == KEYDOWN:
-            eindein.andar()
-    
-    todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
+            if event.key == K_SPACE:
+                eindein.pular()
+
+    teclas = pygame.key.get_pressed()
+
+    if teclas[K_a]:
+        eindein.mover("esquerda")
+
+    elif teclas[K_d]:
+        eindein.mover("direita")
+
+    tela.blit(fundo_img, (0,0))
+    sprites.draw(tela)
+    sprites.update()
     pygame.display.flip()
--- Scripts/player.py
+++ Scripts/player.py
@@ -5,31 +5,36 @@ from sys import exit
 pygame.init()
 
 class Eindein(pygame.sprite.Sprite):
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append(pygame.image.load("main.py\Assets\Sprites\Player\Eidein-andando0.png"))
-        self.sprites.append(pygame.image.load("main.py\Assets\Sprites\Player\Eidein-andando1.png"))
-        self.sprites.append(pygame.image.load("main.py\Assets\Sprites\Player\Eidein-andando2.png"))
-        self.sprites.append(pygame.image.load("main.py\Assets\Sprites\Player\Eidein-andando3.png"))
-        self.sprites.append(pygame.image.load("main.py\Assets\Sprites\Player\Eidein-andando4.png"))
-        self.atual = 0
-        self.image = self.sprites[self.atual]
-        self.image = pygame.transform.scale(self.image, (32 * 10, 32 * 10))
+
+        self.image = pygame.image.load("Assets/Sprites/Player/Eidein-parado.png")
+        self.image = pygame.transform.scale(self.image, (32 * 5, 32 * 5))
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = 100, 100
+        self.rect.topleft = (100, 100)
 
-        self.animar = False
-
-    def andar(self):
-            self.animar = True
+        self.vel_y = 0
+        self.pulando = False
+        self.gravidade = 2
+        self.velocidade = 8
 
     def update(self):
-        if self.animar == True:
-            self.atual = self.atual + 0.25
-            if self.atual >= len(self.sprites):
-                self.atual = 0
-                self.animar = False
-            self.image = self.sprites[int(self.atual)]
-            self.image = pygame.transform.scale(self.image, (32 * 10, 32 * 10))
+        self.vel_y += self.gravidade
+        self.rect.y += self.vel_y
+
+        if self.rect.bottom >= 420:
+            self.rect.bottom = 420
+            self.pulando = False
+
+    def mover(self, direcao):
+        if direcao == "esquerda":
+            self.rect.x -= self.velocidade
+        elif direcao == "direita":
+            self.rect.x += self.velocidade
+
+    def pular(self):
+        if not self.pulando:
+            self.vel_y = -25
+            self.pulando = True
