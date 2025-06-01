e28c63dd9dac7b941bc3a16d7755814d3bab5765
Rafael-Fernandes314 <rafaelfernandesjp@gmail.com>
2025-06-01 17:29:28 -0300

    Movimentação concluída com alguns bugs

--- Scripts/player.py
+++ Scripts/player.py
@@ -15,31 +15,28 @@ class Eindein(pygame.sprite.Sprite):
         self.vel_y = 0
         self.pulando = False
         self.gravidade = 2
-        self.velocidade = 5
+        self.velocidade = 3
         self.animar = False
 
         self.sprite_e:List[Eindein] = []
-        self.sprite_e.append(pygame.transform.scale(pygame.image.load("Assets/Sprites/Player/Eidein-parado-e.png"), (32 * 4, 32 * 4)))
-        self.sprite_e.append(pygame.transform.scale(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e1.png"), (32 * 4, 32 * 4)))
-        self.sprite_e.append(pygame.transform.scale(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e2.png"), (32 * 4, 32 * 4)))
-        self.sprite_e.append(pygame.transform.scale(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e3.png"), (32 * 4, 32 * 4)))
-        self.sprite_e.append(pygame.transform.scale(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e4.png"), (32 * 4, 32 * 4)))
-        self.atual1 = 0
-        self.image = self.sprite_e[self.atual1]
-
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (100, 100)
-
         self.sprite_d:List[Eindein] = []
+        self.sprite_e.append(pygame.image.load("Assets/Sprites/Player/Eidein-parado-e.png"))
+        self.sprite_e.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e1.png"))
+        self.sprite_e.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e2.png"))
+        self.sprite_e.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e3.png"))
+        self.sprite_e.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-e4.png"))
+
         self.sprite_d.append(pygame.image.load("Assets/Sprites/Player/Eidein-parado-d.png"))
         self.sprite_d.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-d1.png"))
         self.sprite_d.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-d2.png"))
         self.sprite_d.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-d3.png"))
         self.sprite_d.append(pygame.image.load("Assets/Sprites/Player/Eidein-andando-d4.png"))
+
+        self.atual1 = 0
         self.atual2 = 0
+        self.image = self.sprite_e[self.atual1]
         self.image = self.sprite_d[self.atual2]
         self.image = pygame.transform.scale(self.image, (32 * 4, 32 * 4))
-
         self.rect = self.image.get_rect()
         self.rect.topleft = (100, 100)
 
@@ -50,28 +47,29 @@ class Eindein(pygame.sprite.Sprite):
         if self.rect.bottom >= 515:
             self.rect.bottom = 515
             self.pulando = False
-        if self.animar == True:
-            self.atual1 = self.atual1 + 0.2
-            if self.atual1 >= len(self.sprite_e):
-                self.atual1 = 0
-                self.animar = False
-            self.image = self.sprite_e[int(self.atual1)]
-            self.image = pygame.transform.scale(self.image, (32 * 4, 32 * 4))
-
-            self.atual2 = self.atual2 + 0.2
-            if self.atual2 >= len(self.sprite_d):
-                self.atual2 = 0
-                self.animar = False
-            self.image = self.sprite_d[int(self.atual2)]
-            self.image = pygame.transform.scale(self.image, (32 * 4, 32 * 4))
+        
 
     def mover(self, direcao):
         self.animar = True
         if direcao == "esquerda":
             self.rect.x -= self.velocidade
+
+            self.atual1 = self.atual1 + 0.15
+            if self.atual1 >= len(self.sprite_e):
+                self.atual1 = 0
+                self.animar = False
+            self.image = self.sprite_e[int(self.atual1)]
+            self.image = pygame.transform.scale(self.image, (32 * 4, 32 * 4))
             
         elif direcao == "direita":
             self.rect.x += self.velocidade
+
+            self.atual2 = self.atual2 + 0.15
+            if self.atual2 >= len(self.sprite_d):
+                self.atual2 = 0
+                self.animar = False
+            self.image = self.sprite_d[int(self.atual2)]
+            self.image = pygame.transform.scale(self.image, (32 * 4, 32 * 4))
             
 
     def pular(self):
